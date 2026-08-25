ec278b0cdaed5186c22243209f2d0a4a48d91af3
Jonathan <Tigroux07150@outlook.fr>
2026-08-25 03:05:34 +0200

    Remplacement du texte du header par le logo

--- frontend_artisan/src/components/Header.js
+++ frontend_artisan/src/components/Header.js
@@ -5,19 +5,17 @@ const Header = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
       <div className="container">
-        <Link className="navbar-brand fw-bold" to="/">Trouve Ton Artisan</Link>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav ms-auto">
-            <li className="nav-item"><Link className="nav-link" to="/">Accueil</Link></li>
-            <li className="nav-item"><Link className="nav-link" to="/artisans?category=1">Bâtiment</Link></li>
-            <li className="nav-item"><Link className="nav-link" to="/artisans?category=2">Alimentation</Link></li>
-            <li className="nav-item"><Link className="nav-link" to="/artisans?category=3">Fabrication</Link></li>
-            <li className="nav-item"><Link className="nav-link" to="/artisans?category=4">Services</Link></li>
-          </ul>
-        </div>
+        {/* Remplacement du texte par ton logo */}
+        <Link to="/" className="navbar-brand">
+          <img 
+            src={`${process.env.PUBLIC_URL}/Logo.png`} 
+            alt="Logo Trouve Ton Artisan" 
+            style={{ height: '40px' }} // Tu peux ajuster la hauteur selon tes besoins
+          />
+        </Link>
+
+        {/* Le reste de ton menu (liens Accueil, Bâtiment, etc.) */}
+        {/* ... */}
       </div>
     </nav>
   );
